9c151c4adbbcab4f7c9982702eedf10098c9f475
JacobG321 <jacobgruber1998@gmail.com>
2022-07-06 17:24:35 -0400

    crud ready

--- friend.py
+++ friend.py
@@ -10,20 +10,47 @@ class Friend:
         self.created_at = data['created_at']
         self.updated_at = data['updated_at']
     # Now we use class methods to query our database
+    #how we display information
     @classmethod
     def get_all(cls):
         query = "SELECT * FROM friends;"
         # make sure to call the connectToMySQL function with the schema you are targeting.
         results = connectToMySQL('first_flask').query_db(query)
         # Create an empty list to append our instances of friends
-        friends = []
-        # Iterate over the db results and create instances of friends with cls.
-        for friend in results:
-            friends.append( cls(friend) )
-        return friends
+        if len(results) == 0:
+            return []
+        else:
+            friends = []
+            # Iterate over the db results and create instances of friends with cls.
+            for friend in results:
+                friends.append( cls(friend) )
+                #cls is place holder for Friend class, we are creating a friend object inside of the list
+            return friends
 
+    #this is how we add to the Database
     @classmethod
     def save(cls, data ):
-        query = "INSERT INTO friends ( first_name , last_name , occupation , created_at, updated_at ) VALUES ( %(fname)s , %(lname)s , %(occ)s , NOW() , NOW() );"
+        query = "INSERT INTO friends ( first_name , last_name , occupation , created_at, updated_at ) VALUES ( %(first_name)s , %(last_name)s , %(occupation)s , NOW() , NOW() );"
         # data is a dictionary that will be passed into the save method from server.py
         return connectToMySQL('first_flask').query_db( query, data )
+        #the s's are for string
+
+    @classmethod
+    def get_one_friend(cls, data):
+        query = "SELECT * FROM friends WHERE id = %(id)s;"
+        results = connectToMySQL('first_flask').query_db(query, data)
+        if len(results) == 0:
+            return None #can only return one item at most
+        else:
+            #create instance froma  dictionary at index 0 in the list of dictionaries called results
+            return cls(results[0]) #need [0] because "results" is a list of dictionaries, but we a need a dictionary, which is at index 0
+
+    @classmethod
+    def edit_friend(cls, data):
+        query = "UPDATE friends SET first_name = %(first_name)s, last_name = %(last_name)s, occupation = %(occupation)s WHERE id = %(id)s;"
+        return connectToMySQL('first_flask').query_db(query, data)
+
+    @classmethod
+    def delete_friend(cls, data):
+        query = "DELETE FROM friends WHERE id = %(id)s;"
+        return connectToMySQL('first_flask').query_db(query, data)
--- server.py
+++ server.py
@@ -1,3 +1,4 @@
+
 from flask import Flask, render_template, request, redirect
 # import the class from friend.py
 from friend import Friend
@@ -5,31 +6,72 @@ app = Flask(__name__)
 
 @app.route("/")
 def index():
+    #this page shows all friends
     # call the get all classmethod to get all friends
     friends = Friend.get_all()
     print(friends)
+    #put the friend.getall method in a variable that has a list of objects
     return render_template("index.html", all_friends=friends)
             
 
-
-
+#invisible route
 @app.route('/create_friend', methods=["POST"])
 def create_friend():
     # First we make a data dictionary from our request.form coming from our template.
     # The keys in data need to line up exactly with the variables in our query string.
     data = {
-        "fname": request.form["fname"],
-        "lname" : request.form["lname"],
-        "occ" : request.form["occ"]
+        "first_name": request.form["first_name"],
+        "last_name" : request.form["last_name"],
+        "occupation" : request.form["occupation"]
     }
     # We pass the data dictionary into the save method from the Friend class.
     Friend.save(data)
     # Don't forget to redirect after saving to the database.
-    return redirect('/add_friend')
+    return redirect('/')
 
+#viewable route
 @app.route('/add_friend')
 def add_friend():
     return render_template('create.html')
+
+#view one page, viewable route
+@app.route('/friends/view/<int:id>')
+def view_info(id):
+    data = {
+        "id" : id #need id for query
+    }
+    return render_template('view_one_friend.html', this_friend = Friend.get_one_friend(data) )
+
+#edit #visible route
+@app.route('/friends/edit/<int:id>')
+def edit(id):
+    data = {
+        "id" : id #need id for query
+    }
+    return render_template('edit_friend.html', this_friend = Friend.get_one_friend(data) )
+
+#edit #invRoute
+@app.route('/friends/<int:id>/edit_friend_in_db', methods=["POST"])
+def edit_friend_in_db(id):
+    data = {
+        "first_name": request.form["first_name"],
+        "last_name" : request.form["last_name"],
+        "occupation" : request.form["occupation"],
+        "id" : id #need this to update this specific zoo
+    }
+    # We pass the data dictionary into the save method from the Friend class.
+    Friend.edit_friend(data)
+    return redirect(f'/friends/view/{id}')
+
+
+#delete
+@app.route('/friends/delete/<int:id>')
+def delete(id):
+    data = {
+        "id": id
+    }
+    Friend.delete_friend(data)
+    return redirect('/')
 
 
 
